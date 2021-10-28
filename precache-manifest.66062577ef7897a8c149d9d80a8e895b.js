a3b6ee6c65e29cd1af3fb31d0657ad898c394778
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-28 21:49:53 +0000

    Deploy jungleGary/Comp3104 to github.com/jungleGary/Comp3104.git:gh-pages

---
+++ precache-manifest.66062577ef7897a8c149d9d80a8e895b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a34c90f36d1e112148ac",
+    "url": "/jungleGary/COMP3104/static/css/main.0aba520f.chunk.css"
+  },
+  {
+    "revision": "a34c90f36d1e112148ac",
+    "url": "/jungleGary/COMP3104/static/js/main.a34c90f3.chunk.js"
+  },
+  {
+    "revision": "a9875794f69f8d80181b",
+    "url": "/jungleGary/COMP3104/static/js/runtime~main.a9875794.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/jungleGary/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "0a7bb07a5eead915a7c83b87626a174a",
+    "url": "/jungleGary/COMP3104/index.html"
+  }
+];
